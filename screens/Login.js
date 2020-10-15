e28d9d645b92619a3dccb25a003880e4361ef1d6
David Eriksson <david_ux@outlook.com>
2020-10-15 10:03:57 +0200

    Added font-library Jost
    Fonts can be imported from `@expo-google-fonts/{font-name}` like this:
    
    `import { useFonts, Jost_600SemiBold } from "@expo-google-fonts/jost";`
    
    Make sure to check state of the `useFonts` hook before loading view, if unsure, look through `Login.js`
    `

--- screens/Login.js
+++ screens/Login.js
@@ -1,44 +1,100 @@
 import * as React from "react";
-import { StyleSheet, Text, Button, View, SafeAreaView } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  Button,
+  View,
+  SafeAreaView,
+  TextInput,
+} from "react-native";
+import { AppLoading } from "expo";
 import Buildings from "../resources/svg/buildings.js";
+import { useFonts, Jost_600SemiBold } from "@expo-google-fonts/jost";
 
 const Login = ({ navigation }) => {
+  const [emailValue, onChangeTextEmail] = React.useState("");
+  const [passwordValue, onChangeTextPassword] = React.useState("");
+  const [fontsLoaded] = useFonts({
+    Jost_600SemiBold,
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
   return (
     <SafeAreaView style={styles.loginWrapper}>
-      <Text style={styles.headerText}>APARTYMENT</Text>
-      <Buildings style={styles.buildings} />
-      <View style={styles.loginForm}>
-        <Button
-          style={styles.loginButton}
-          title="LOGIN"
-          onPress={() => navigation.navigate("Hub")}
+      <View style={styles.headerView}>
+        <Text style={styles.headerText}>APARTYMENT</Text>
+      </View>
+      <View style={styles.imageView}>
+        <Buildings />
+      </View>
+      <View style={styles.formView}>
+        <Text style={styles.formTitle}>Email address</Text>
+        <TextInput
+          placeholder=" you@email.com"
+          selectionColor="#F72585"
+          style={styles.formInputField}
+          onChangeText={(text) => onChangeTextEmail(text)}
+          value={emailValue}
         />
+        <Text style={styles.formTitle}>Password</Text>
+        <TextInput
+          placeholder=" Your Password"
+          secureTextEntry={true}
+          selectionColor="#F72585"
+          style={styles.formInputField}
+          onChangeText={(text) => onChangeTextPassword(text)}
+          value={passwordValue}
+        />
+        <View style={styles.buttonWrapper}>
+          <Button title="Sign In" color="#F72585" />
+        </View>
       </View>
     </SafeAreaView>
   );
 };
 
-// Styling example. If an inline style takes more than
-// one(1) parameters, use this instead:
-
 const styles = StyleSheet.create({
   loginWrapper: {
     justifyContent: "space-between",
     alignItems: "center",
+    flex: 1,
+    flexDirection: "column",
+    backgroundColor: "#FAFAFA",
+  },
+
+  headerView: {
     marginTop: 100,
+    flex: 1,
+  },
+  imageView: {
+    flex: 2,
+  },
+  formView: {
+    flex: 3,
   },
 
   headerText: {
     color: "#F72585",
+    fontFamily: "Jost_600SemiBold",
+    fontSize: 36,
   },
 
-  buildings: {
-    padding: 50,
+  formTitle: {
+    fontFamily: "Jost_600SemiBold",
   },
 
-  loginForm: { height: 50, color: "#2119AB" },
+  formInputField: {
+    height: 50,
+    width: 300,
+    borderRadius: 5,
+    backgroundColor: "#EFEFEF",
+  },
 
-  loginButton: { color: "#F72585" },
+  buttonWrapper: {
+    marginTop: 10,
+  },
 });
 
 export default Login;
